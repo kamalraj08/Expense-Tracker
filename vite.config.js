4d5c97ecefb34dbe239916cbb201257aac873ff0
kamalraj08 <kamalraj2k24@gmail.com>
2025-03-08 18:16:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,21 @@ export default defineConfig({
   base: "/Expense-Tracker/", // Change to your repo name
   build: {
     outDir: "dist",
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000KB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "react-vendor"; // Separate React-related code
+            }
+            if (id.includes("recharts")) {
+              return "recharts-vendor"; // Separate recharts code
+            }
+            return "vendor"; // Split general dependencies
+          }
+        },
+      },
+    },
   },
 })
