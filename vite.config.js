226483e1537ff69bdc6e1d873db886b386df8bf2
kamalraj08 <kamalraj2k24@gmail.com>
2025-03-08 18:10:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/expense-tracker/'
+  base: "/Expense-Tracker/", // Change to your repo name
+  build: {
+    outDir: "dist",
+  },
 })
